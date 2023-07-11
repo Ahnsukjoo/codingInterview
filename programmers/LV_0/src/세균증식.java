29eb7804b343b2a2f3f9c723a2c1ff10e1253cf4
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2023-07-11 11:44:47 +0900

    add : 세균증식

---
+++ programmers/LV_0/src/세균증식.java
@@ -0,0 +1,12 @@
+public class 세균증식 {
+
+    public int solution(int n, int t) {
+        int answer = n;
+        for (int i = 1; i <= t; i++) {
+            answer = answer * 2;
+        }
+        return answer;
+
+    }
+
+}
